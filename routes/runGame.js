9c0168a028f0868077b5d3c8b9c00102e053f646
mefuru <mehul.mandania@gmail.com>
2013-12-04 23:00:13 -0500

    removed possibility of duplicate songs in question

--- routes/runGame.js
+++ routes/runGame.js
@@ -1,72 +1,55 @@
 ;
 var __ = require("underscore");
 var async = require("async");
-var crypto = require("crypto");
 var MongoClient = require("mongodb").MongoClient;
+var crypto = require("crypto");
+
 exports.runGame = function(req, res) {
     var rapperName = req.body.artist;
-    var fourLines = [];
-    var fourSongNames = [];
     MongoClient.connect("mongodb://localhost:27017/rapGeniusData", function(err, db) {
         if(err) throw err;
-        var collection = db.collection("songs");
-        var collection2 = db.collection("answers");
-        var correctAnswer = "";
+        var songs = db.collection("songs");
         var query = {artist: rapperName};
-        collection.count({artist: rapperName}, function(err, count) {
-            async.series([
-                function(seriesCallback) { getLyrics(count, collection, query, fourLines, correctAnswer, fourSongNames, db, seriesCallback); },
-                function(seriesCallback) { getRandomSongNames(count, collection, query, fourSongNames, db, seriesCallback); },
-                function(seriesCallback) { getRandomSongNames(count, collection, query, fourSongNames, db, seriesCallback); },
-                function(seriesCallback) { getRandomSongNames(count, collection, query, fourSongNames, db, seriesCallback); }
-            ],
-            function(err,results) {
-                if(err) throw err;
-                saveAnswer(fourLines, fourSongNames, results[0], collection2, db, res);
-                console.log(results);
-            });
+        songs.find({artist: rapperName}, {name: 1, lyrics: 1, _id: 0}).toArray(function(err, docs) {
+            getFourRandomSongTitles(docs, getFourRandomLines, db, res);
         });
     });
 };
 
-//  Get lyrics and one song name
-var getLyrics = function(count, collection, query, fourLines, correctAnswer, fourSongNames, db, seriesCallback) {
-    var options = {skip: Math.floor((Math.random()*(count-1))), limit: 1};
-    collection.find(query, options).toArray(function(err, docs) {
-        if(err) seriesCallback(err, null);
-        var max = docs[0].lyrics.length - 4;
-        var min = 0;
-        var firstLineIndex = Math.floor(Math.random() * (max - min + 1) + min);
-        for (var i = 0; i < 4; i++) {
-            fourLines.push(docs[0].lyrics[firstLineIndex+i]);
+// get four song names
+var getFourRandomSongTitles = function(docs, getFourRandomLines, db, res) {
+    var arr = []; // will contain four random indexes representing four songs
+    while(arr.length < 4){ // populate arr
+        var randomnumber = Math.ceil(Math.random()*(docs.length-1))
+        var found = false;
+        for(var i = 0; i<arr.length; i++){
+            if (arr [i] == randomnumber) {found = true;break}
         }
-        correctAnswer = docs[0].name;
-        fourSongNames.push(correctAnswer);
-        db.close;
-        seriesCallback(null, correctAnswer);
-    });
+        if(!found)arr[arr.length]=randomnumber;
+    }
+    var songNames = __.map(arr, function(songIndex) {return docs[songIndex].name});
+    getFourRandomLines(docs, db, songNames, arr, res);
 };
 
-// Get random song names from the artist to populate question
-var getRandomSongNames = function(count, collection, query, fourSongNames, db, seriesCallback) {
-    var options = {skip: Math.floor((Math.random()*(count-1))), limit: 1}; // fn 2
-    collection.find(query, options).toArray(function(err, docs) {
-        if(err) seriesCallback(err, null);
-        fourSongNames.push(docs[0].name);
-        db.close;
-        seriesCallback(null, docs[0].name);
-    });
+// get four lines from a song
+var getFourRandomLines = function(docs, db, songNames, arr, res) {
+    var fourLines = []; // lyrics to give to lines
+    var correctIndex = Math.floor(Math.random() * (4)); // correctAnswer. Between 0 and 3
+    var max = docs[arr[correctIndex]].lyrics.length - 4;
+    var firstLineIndex = Math.floor(Math.random() * (max + 1));
+    var lineIndexes = [firstLineIndex, firstLineIndex+1, firstLineIndex+2, firstLineIndex+3];
+    var fourLines = __.map(lineIndexes, function(lineIndex) {return docs[arr[correctIndex]].lyrics[lineIndex]});
+    saveAnswer(fourLines, songNames, correctIndex, db, res);
 };
 
 // Save answer to DB and render page with question
-var saveAnswer = function(fourLines, fourSongNames, correctAnswer, collection2, db, res) {
+var saveAnswer = function(fourLines, songNames, correctIndex, db, res) {
     var hex = crypto.createHash('sha1').update(fourLines.join()).digest('hex');
-    var possibleAnswers = __.shuffle(fourSongNames);
-    var correctIndex = __.indexOf(possibleAnswers, correctAnswer);
     var answer = {sha1: hex, answer: correctIndex};
-    collection2.insert(answer, function(err, docs) {
+    var answers = db.collection("answers");        
+    answers.insert(answer, function(err, docs) {
         if(err) seriesCallback(err, null);
-        db.close;
-        res.render("game", {fourLines: fourLines, songNames: possibleAnswers, sha1: hex});
+        db.close();
+        res.render("game", {fourLines: fourLines, songNames: songNames, sha1: hex});
     });
 };
