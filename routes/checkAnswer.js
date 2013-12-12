fb9b6e6f17d24d659b0a83a812d2cc17ad094c53
mefuru <mehul.mandania@gmail.com>
2013-12-12 15:28:39 -0500

    refactored checkAns

--- routes/checkAnswer.js
+++ routes/checkAnswer.js
@@ -1,13 +1,13 @@
 var MongoClient = require("mongodb").MongoClient;
 var async = require("async");
+var __ = require("underscore");
 
 exports.checkAnswer = function(req, res) {
     MongoClient.connect("mongodb://localhost:27017/rapGeniusData", function(err, db) {
         if(err) throw err;
         var answerCollection = db.collection("answers");
         var userInput = [req.body.songName1, req.body.songName2, req.body.songName3, req.body.songName4, req.body.songName5];
-        var userAnswers = [];
-        var queries = [];
+        var userAnswersAndQueries = [];
         var answersLog = {};
         var numCorrectAnswers = 0;
         var checkAgainstDB = function(query, userAnswer, parallelCallback) {
@@ -21,32 +21,20 @@ exports.checkAnswer = function(req, res) {
                 }
             });
         };
-        
+        // populate users answers and queries from user input
         for (var i = 0; i < userInput.length; i++) {
-            userAnswers.push(userInput[i].substr(userInput[i].length - 1));
-            queries.push({sha1: userInput[i].substr(0, userInput[i].length - 1)});
+            userAnswersAndQueries.push({
+                userAnswer: userInput[i].substr(userInput[i].length - 1),
+                query:{sha1: userInput[i].substr(0, userInput[i].length - 1)}
+            });
         }
-        async.parallel([
-            function(parallelCallback) {
-                checkAgainstDB(queries[0], userAnswers[0], parallelCallback);
-            },
-            function(parallelCallback) {
-                checkAgainstDB(queries[1], userAnswers[1], parallelCallback);
-            },
-            function(parallelCallback) {
-                checkAgainstDB(queries[2], userAnswers[2], parallelCallback);
-            },
-            function(parallelCallback) {
-                checkAgainstDB(queries[3], userAnswers[3], parallelCallback);
-            },
-            function(parallelCallback) {
-                checkAgainstDB(queries[4], userAnswers[4], parallelCallback);
+        // for each question check answer against db and then render
+        async.parallel(__.map(userAnswersAndQueries, function(userAnswerAndQuery) {
+            return function(parallelCallback) {
+                checkAgainstDB(userAnswerAndQuery.query, userAnswerAndQuery.userAnswer, parallelCallback);
             }
-        ], function(err, results) {
-            console.log(err, results);
-            console.log(numCorrectAnswers);
-            res.render("checkAnswer", {correctAnswers: numCorrectAnswers, results: results});                               
-        }
+        }), function(err, results) {
+              res.render("checkAnswer", {correctAnswers: numCorrectAnswers, results: results});                }
         );
     });    
 };
