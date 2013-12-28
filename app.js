caa16765724d63172470e756432a1df1b35ee143
mefuru <mehul.mandania@gmail.com>
2013-12-28 17:06:15 +0000

    test

--- app.js
+++ app.js
@@ -15,8 +15,7 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 app.engine('html', cons.swig);
-app.use(express.bodyParser()); // required for POST requests
-app.use(express.favicon());
+app.use(express.bodyParser());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
